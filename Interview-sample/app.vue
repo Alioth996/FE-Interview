164c4fce962d8a06ddf5da6cfc081c4403f5cd7d
alex <2844520415@qq.com>
2023-02-21 22:19:36 +0800

    chore: add nuxt3 code sample

---
+++ Interview-sample/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" p="3" font="mono">
+    <header>
+      <h1 text="blue-400 center 4xl">前端面试--代码实操</h1>
+    </header>
+    <section>
+      <!-- 代码演示区 -->
+    </section>
+
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ Interview-sample/windi.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  /* 属性书写模式 */
+  attributify: true
+})
